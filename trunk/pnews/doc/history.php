22039b2fa3c9dc58aa22632044d22d0a7fa86622
cdsheen <cdsheen@fe2ed6f1-d525-0410-ac78-8749a169f707>
2003-07-04 03:47:14 +0000

    prepare for 2.2.2
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@164 fe2ed6f1-d525-0410-ac78-8749a169f707

--- trunk/pnews/doc/history.php
+++ trunk/pnews/doc/history.php
@@ -48,9 +48,9 @@ echo <<<EOH
 <!--
 <li>RFC 2046 - MIME multipart document and attachment download support. (not yet)
 -->
-PHP News Reader v2.2.2 (2003/06/29)
+PHP News Reader v2.2.2 (2003/07/05)
 <ul>
-<li>Option to force using SSL(HTTPS) when performing login. (<a href=guide.php#https_login)\$CFG["https_login"]</a>)
+<li>Option to force using SSL(HTTPS) when performing login. (<a href=guide.php#https_login>)\$CFG["https_login"]</a>)
 <li><a href=guide.php#url_base>\$CFG["url_base"]</a> must be configured even if <a href=guide.php#url_rewrite>\$CFG["url_rewrite"]</a> is off
 </ul>
 PHP News Reader v2.2.1 (2003/06/21)
