90a331392b0fabe787d350a4e40b26849d0b0762
cdsheen <cdsheen@fe2ed6f1-d525-0410-ac78-8749a169f707>
2003-07-21 05:47:27 +0000

    add v2.2.3
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@177 fe2ed6f1-d525-0410-ac78-8749a169f707

--- trunk/pnews/doc/history.php
+++ trunk/pnews/doc/history.php
@@ -51,7 +51,7 @@ echo <<<EOH
 -->
 PHP News Reader v2.2.3 (2003/07/20)
 <ul>
-<li>Bug in replying if the quoted article has leading spaces.
+<li>Bug at replying if the quoted article has leading spaces.
 </ul>
 PHP News Reader v2.2.2 (2003/07/05)
 <ul>
