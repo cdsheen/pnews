cafedac7aaed39c131530a4fe5e00e3c3887c50c
cdsheen <cdsheen@fe2ed6f1-d525-0410-ac78-8749a169f707>
2003-08-06 10:58:47 +0000

    allow # in url highlight
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@195 fe2ed6f1-d525-0410-ac78-8749a169f707

--- trunk/pnews/nnrp.inc.php
+++ trunk/pnews/nnrp.inc.php
@@ -283,7 +283,7 @@ function nnrp_body ( $nhd, $artnum, $prepend = "", $postpend = "", $urlquote = t
 
 		if( $urlquote ) {
 			# replace url as anchor
-			$buf = preg_replace( '/(((http)|(ftp)|(https)):\/\/([\w\d-_.:\/~+=?,]|(&amp;))+)/', ' <a href="\\1" target=_blank>\\1</a>', $buf );
+			$buf = preg_replace( '/(((http)|(ftp)|(https)):\/\/([\w\d-_.:\/~+=?,#]|(&amp;))+)/', ' <a href="\\1" target=_blank>\\1</a>', $buf );
 
 			# replace e-mail as anchor
 			$buf = preg_replace( '/([\w\d-_.]+)@([\w\d-_.]+)/', ' <a href="mailto:\\1@\\2" target=_blank>\\1@\\2</a>', $buf );
