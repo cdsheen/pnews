ea3cf81d779e9477d4e04f5d24d8cd0405f96121
cdsheen <cdsheen@fe2ed6f1-d525-0410-ac78-8749a169f707>
2003-09-02 08:18:01 +0000

    add french language definition
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@207 fe2ed6f1-d525-0410-ac78-8749a169f707

---
+++ trunk/pnews/doc/acknowlege.php
@@ -0,0 +1,59 @@
+<?
+
+# PHP News Reader
+# Copyright (C) 2001-2003 Shen Chang-Da
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+require_once('../version.inc.php');
+
+if( $_SERVER['HTTPS'] )
+	$sflogo = 'https://sourceforge.net/sflogo.php?group_id=71412&amp;type=1';
+else
+	$sflogo = 'http://sourceforge.net/sflogo.php?group_id=71412&amp;type=1';
+
+echo <<<EOH
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=big5">
+<LINK REL=STYLESHEET TYPE="text/css" HREF="style.css">
+<title>PHP News Reader</title>
+</head>
+<body style="background-color: #EEFFFF">
+<table width=100% cellpadding=0 cellspacing=0><tr><td>
+ <font face="Georgia"><h3>$pnews_name $pnews_version</h3></font>
+</td><td align=right valign=bottum>
+<font face="Georgia" size=1>Release Date: $pnews_release</font>
+</td></tr></table>
+<font size=3 color=black face="Georgia">
+Acknowledgement
+<hr>
+<ul>
+<li>Pascal Aubry - French translation, CAS authentication and phpCAS library
+</ul>
+<hr><table width=100% cellspacing=0 cellpadding=0><tr><td><font size=2>$pnews_claim</font><br>
+<a href=http://sourceforge.net/projects/pnews/ target=_blank>http://sourceforge.net/projects/pnews/</a>
+</td><td align=right>
+<a href="http://sourceforge.net/" alt="http://sourceforge.net/" target=_blank>
+<img src="$sflogo" border="0" alt="SourceForge.net Logo">
+</a>
+</td></tr></table>
+</font>
+</body>
+</html>
+
+EOH;
+?>
---
+++ trunk/pnews/language/french.inc.php
@@ -0,0 +1,122 @@
+<?
+
+// PHP News Reader - Language definition
+
+// Language: French
+// Coding:   ISO-8859-1
+
+// Copyright (C) 2001-2003 - All rights reserved
+// Pascal Aubry (Pascal.Aubry@univ-rennes1.fr)
+
+$strAuthentication     = 'Authentification';
+$strLoginName          = 'Utilisateur';
+$strPassWord           = 'Mot de passe';
+$strNeedLogin          = 'La page demand&eacute;e n&eacute;cessite une authentification';
+$strUseYourAccountAt   = 'Veuillez utiliser votre compte <b>%s</b> pour vous connecter&nbsp;:';
+$strLogin              = 'Connexion';
+$strLogout             = 'D&eacute;connexion';
+
+$strHTTPlogoutInfo     = 'Veuillez fermer toutes les fentres de votre navigateur pour &eacute;viter les acc&egrave;s non autorisés.';
+
+$strAuthExpired        = 'Expiration de votre authentification. Vous devez vous reconnecter&nbsp;!';
+
+$strPasswordRetryFail  = 'L\'utilisateur et le mot de passe fournis sont incorrects';
+$strReLogin            = 'Reconnexion';
+
+$strGoBack             = 'Retour';
+
+$strNewsServer         = 'Serveur de forums';
+$strConnectServerError = 'La connexion au serveur de news a &eacute;chou&eacute;.';
+$strReadonlyNotify     = 'Lecture seule';
+
+$strGroupList          = 'Liste des groupes';
+
+$strSessionError       = 'Erreur d\'acc&egrave;s';
+$strSessionErrorReason = 'Erreur d\'acc&egrave;s à %s - lecture seule ou probl&egrave;me d\'expiration';
+$strSessionErrorReturn = 'Veuillez retourner à «&nbsp;%s&nbsp;» et essayer de nouveau';
+
+$strNumber             = 'N°';
+$strPostNumber         = 'Art.';
+$strGroup              = 'Forum';
+$strGroupDescription   = 'Description';
+$strSubject            = 'Sujet';
+$strAuthor             = 'Auteur';
+$strTime               = 'Date';
+$strAction             = 'Action';
+$strReceiver           = 'Destinataire';
+$strName               = 'Nom';
+$strEmail              = 'Mél';
+$strContent            = 'Corps du message';
+
+$strNoSubject          = '<pas de sujet>';
+
+$strRefresh            = 'Rafra&icirc;chir';
+
+$strReply              = 'R&eacute;pondre';
+$strDelete             = 'Effacer';
+$strCrossPost          = 'Envoi group&eacute;';
+$strForward            = 'Envoyer';
+
+$strPost               = 'Poster';
+$strReplyDetail        = 'R&eacute;pondre';
+$strReplyQuoteDetail   = 'R&eacute;pondre(inclure message)';
+$strDeleteDetail       = 'Effacer';
+$strCrossPostDetail    = 'Envoi group&eacute;';
+$strForwardDetail      = 'Envoyer';
+
+$strCloseWindow        = 'Fermer';
+
+$strFirstPage          = 'Premier';
+$strLastPage           = 'Dernier';
+$strNextPage           = 'Suivant';
+$strPreviousPage       = 'Pr&eacute;c&eacute;dent';
+
+$strNextArticle        = 'Suivant';
+$strLastArticle        = 'Pr&eacute;c&eacute;dent'; 
+
+$strPageNumber         = 'Page %s/%s';
+
+$strReturnToGroupList  = 'Liste des groupes';
+$strReturnToIndexing   = 'Liste des articles';
+$strNoPostPermission   = 'Vous n\'avez pas la permission de poster un article dans ce groupe';
+$strNoArticle          = 'Il n\'y a aucun article dans ce groupe';
+
+$strArticlePosted      = 'L\'article suivant a &eacute;t&eacute; post&eacute;';
+$strArticleIsDeleted   = 'L\'article suivant a &eacute;t&eacute; effac&eacute;';
+$strArticleIsForwarded = 'L\'article suivant a &eacute;t&eacute; envoy&eacute; ';
+
+$strRealyQuit          = 'Voulez-vous vraiment arr&ecirc;ter l\'&eacute;dition&nbsp;';
+$strRealyDelete        = 'Voulez-vous vraiment effacer cet article&nbsp;?';
+
+$strFormConfirmPost    = 'Poster';
+$strFormCancelPost     = 'Annuler';
+
+$strFormConfirmDelete  = 'Effacer';
+$strFormCancelDelete   = 'Annuler';
+
+$strFormConfirmForward = 'Envoyer';
+$strFormCancelForward  = 'Annuler';
+
+$strFormInsertQuote    = 'Copier le texte';
+
+$strPleaseEnterName    = 'Veuillez entrer votre nom&nbsp;!';
+$strPleaseEnterEmail   = 'Veuillez entrer votre mél&nbsp;!';
+$strPleaseEnterGroup   = 'Veuillez spécifier le groupe&nbsp;!';
+$strPleaseEnterSubject = 'Veuillez indiquer le sujet&nbsp;!';
+$strPleaseEnterContent = 'Veuillez remplir le corps du message&nbsp;!';
+$strPleaseEnterReceiver= 'Veuillez indiquer le destinataire&nbsp;!';
+
+$strNoPostJustMail     = 'Voulez-vous répondre par courrier sans poster&nbsp;?';
+
+$strReplyToAuthor      = 'Répondre à l\'auteur';
+$strNotPostToGroup     = 'Ne pas poster';
+
+$strQuoteFrom          = '== Extrait de l\'article de «&nbsp;%s&nbsp;»';
+$strForwardFrom        = '== Envoyé pas «&nbsp;%s&nbsp;»';
+$strCrossPostAuthor    = '== Renvoi de l\'article de «&nbsp;%s&nbsp;»';
+
+$strPostStatus         = '== Post&eacute; le %s dans %s';
+
+$strMyFavor            = 'Favori';
+
+?>
