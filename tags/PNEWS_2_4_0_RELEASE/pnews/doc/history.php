5a5cff2ad2d3a2cf55c8f9e1dbce4f0ee51f6a78
(no author) <(no author)@fe2ed6f1-d525-0410-ac78-8749a169f707>
2003-10-03 17:27:11 +0000

    This commit was manufactured by cvs2svn to create tag 'PNEWS_2_4_0_RELEASE'.
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@272 fe2ed6f1-d525-0410-ac78-8749a169f707

---
+++ tags/PNEWS_2_4_0_RELEASE/pnews/doc/history.php
@@ -0,0 +1,245 @@
+<?
+
+# PHP News Reader
+# Copyright (C) 2001-2003 Shen Chang-Da
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+require_once('../version.inc.php');
+
+if( $_SERVER['HTTPS'] )
+	$sflogo = 'https://sourceforge.net/sflogo.php?group_id=71412&amp;type=1';
+else
+	$sflogo = 'http://sourceforge.net/sflogo.php?group_id=71412&amp;type=1';
+
+echo <<<EOH
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=big5">
+<LINK REL=STYLESHEET TYPE="text/css" HREF="style.css">
+<title>History of PHP News Reader</title>
+</head>
+<body style="background-color: #EEFFFF">
+<table width=100% cellpadding=0 cellspacing=0>
+<tr>
+<td>
+ <font face="Georgia"><h3>$pnews_name $pnews_version</h3></font>
+ </td>
+<td align=right valign=bottum>
+ <font face="Georgia" size=1>Release Date: $pnews_release</font>
+</td>
+</tr>
+</table>
+<font size=3 color=black face="Georgia">
+<a href=index.php>PHP News Reader</a> Release histroy
+<hr />
+<!--
+<li>RFC 2046 - MIME multipart document and attachment download support. (not yet)
+-->
+PHP News Reader v2.4.0 (2003/10/04)
+<ul>
+<li>Support News Server with NNTP over SSL connection (also known as NNTPS or SNEWS)
+<li>Support <a href=guide.php#nntps_auth>NNTPS</a> authentication module
+<li>Overview format is confirmed by <b>LIST OVERVIEW.FMT</b>
+<li>Caching can be enabled to accelerate the indexing process. (<a href=guide.php#cache_dir>\$CFG["cache_dir"]</a>)
+<li>Check for the invalid access to groups not listed in newsgroups.lst
+<li><a href=guide.php#article_order_reverse>\$CFG["article_order_reverse"]</a> is deprecated, and replaced by <a href=guide.php#show_newest_top>\$CFG["show_newest_top"]</a>
+<li>Fix the problem of filtering ANSI coloring codes.
+</ul>
+PHP News Reader v2.3.1 (2003/09/27)
+<ul>
+<li>Reading article with invalid artnum will be redirected into indexing page.
+<li>Dead news server will not slow down the access for groups in other servers.
+<li>Better error handling when news server is unavailable.
+<li>Remove the read-only indication when <a href=guide.php#post_restriction>\$CFG["post_restriction"]</a> is enabled.
+<li>Fix several missing style sheet errors.
+<li>Auto-correction for some invalid uri if <a href=guide.php#url_rewrite>\$CFG["url_rewrite"]</a> is enabled.
+</ul>
+PHP News Reader v2.3.0 (2003/09/06)
+<ul>
+<li>Added French translation (by Pascal Aubry)
+<li>Support <a href=http://www.yale.edu/tp/auth target=_blank>CAS</a> authentication module (by Pascal Aubry)
+<li>E-Mail editing can be disabled by setting <a href=guide.php#email_editing>\$CFG["email_editing"]</a> as off
+<li>The character '#' is included in the uri of hyperlink auto-detection.
+<li>Support more flexible <a href=guide.php#group_match>group matching syntax</a> in newsgroup.lst.
+<li>Directly linking to the next/previous article
+<li>Language switching reworked, relation between charset and language is more clear.
+<li><a href=guide.php#interface_language>\$CFG["interface_language"]</a> makes the decision at the language used in interface.
+</ul>
+PHP News Reader v2.2.4 (2003/07/26)
+<ul>
+<li>Fix the bug in MySQL authentication module.
+<li>Use of <a href=guide.php#magic_tag>\$CFG["magic_tag"]</a> to indicate the visit state of newsgroup
+</ul>
+PHP News Reader v2.2.3 (2003/07/20)
+<ul>
+<li>Fix the bug at replying if the quoted article has leading spaces.
+</ul>
+PHP News Reader v2.2.2 (2003/07/05)
+<ul>
+<li>Option to force using SSL(HTTPS) when performing login. (<a href=guide.php#https_login>\$CFG["https_login"]</a>)
+<li><a href=guide.php#url_base>\$CFG["url_base"]</a> must be configured even if <a href=guide.php#url_rewrite>\$CFG["url_rewrite"]</a> is off
+</ul>
+PHP News Reader v2.2.1 (2003/06/21)
+<ul>
+<li>Initial support for style sheet configuration via <a href=guide.php#style_sheet>\$CFG["style_sheet"]</a>
+<li>The default news server address can be ignored when <a href=guide.php#url_rewrite>\$CFG["url_rewrite"]</a> is enabled
+<li>Handling quotes correctly with the <i>magic_quotes_gpc</i> state of PHP.
+<li>The leading spaces are displayed as-is while reading articles.
+<li>Article is displayed in 'mono-space' font by default.
+<li>Fix the bug of the configuration checking about NNTP authentication module.
+<li>Fix the group verification error while forwarding article.
+<li>Language switch can be turned off by setting <a href=guide.php#language_switch>\$CFG["language_switch"]</a> as false.
+<li>Fix the lower-case problem of nnrp authentication parameter in newsgroups.lst
+</ul>
+PHP News Reader v2.2.0 (2003/05/25)
+<ul>
+<li>Support news server which requires authorization.
+<li>The ANSI coloring codes are filtered by default (<a href=guide.php#filter_ansi_color>\$CFG["filter_ansi_color"]</a>).
+<li>Support links to Next and Previous articles.
+<li>Support URL rewrite function (<a href=guide.php#url_rewrite>\$CFG["url_rewrite"]</a> and <a href=guide.php#url_base>\$CFG["url_base"]</a>)
+<li>Deleted articles are skipped and exactly 20 articles are displayed in one page
+<li>Add option (<a href=guide.php#article_order_reverse>\$CFG["article_order_reverse"]</a>) to config the article numbering order.
+<li>Add option (<a href=guide.php#show_article_popup>\$CFG["show_article_popup"]</a>) to config the use of popup window.
+<li>Support NNTP authentication module for authenticate with News server.
+<li>Number of articles per page can be configured by <a href=guide.php#articles_per_page>\$CFG["articles_per_page"]</a> (default is 20).
+<li>Fix a bug when post after timeout with "form" style login.
+</ul>
+PHP News Reader v2.1.2 (2003/04/05)
+<ul>
+<li>Do not strip whitespace from the beginning of each line in the article posted.
+<li>Jump to the correct catalog when returning from indexing page.
+<li>Correct the problem of lost for POST variables when launching the login dialog.
+</ul>
+PHP News Reader v2.1.1 (2003/03/08)
+<ul>
+<li>The format of time displayed can be configured by <a href=guide.php#time_format>\$CFG["time_format"]</a>.
+<li>Only show the group name in the title of indexing page.
+<li>Add more error handling codes for invalid news server settting.
+<li>Several author name/mail parsing problems fixed.
+<li>Fix variable missing problem in 'open' authentication type.
+</ul>
+PHP News Reader v2.1.0 (2003/03/02)
+<ul>
+<li>Installation Guide released.
+<li>Use 'charset' instead of 'language' in the configuration file
+<li>The correct charset of group is used while loading pages without session.
+<li>The MIME charset of article will be honored while processing news articles.
+<li>The article will be posted and mailed with correct MIME headers about charset information based on the RFC 2045 standard.
+</ul>
+<p>
+PHP News Reader v2.0.4 (2003/02/18)
+<ul>
+<li>Change the article numbering order (descent)
+</ul>
+<p>
+PHP News Reader v2.0.3 (2003/02/05)
+<ul>
+<li>Use UTF-8 for English locale
+</ul>
+<p>
+PHP News Reader v2.0.2 (2003/01/24)
+<ul>
+<li>Fix the bug of 'required' authentication model
+</ul>
+<p>
+PHP News Reader v2.0.1 (2003/01/22)
+<ul>
+<li>Correct the link of SourceForge logo
+</ul>
+<p>
+PHP News Reader v2.0 (2003/01/19)
+<ul>
+<li>First public release under <a href=copying.php>GPL</a> and hosts on <a href=http://sourceforge.net/ target=_blank>SourceForge</a>
+<li>Rewrite the authentication codes and the use of the ticket system
+<li>Logging out support
+<li>Support 'optional' authentication model for Read-Only access without authentication
+<li>Rewrite the LDAP and POP3 authentication modules
+<li>Support Mail authentication module for multiple POP3 server environment
+<li>Support MySQL and PostgreSQL authentication modules, easy to integrate with <a href="http://www.phpbb.com/" target=_blank>phpBB</a>
+<li>FTP authentication module support
+<li>User information can be extracted from LDAP attributes or Database table fields along with authentication
+<li>Standardize the requirements for writing an authentication module
+<li>Support FORM style login process other than the original HTTP authentication
+<li>More flexible configuration based on a new config.inc.php syntax
+<li>Enable interface language to switch without cookie support
+<li>Different encodings can be used in different catalogs
+<li>Support private groups which only accessible by authenticated user
+<li>Fix the E-Mail parsing bug in XOVER command of NNRP library
+</ul>
+<p>
+PHP News Reader v1.9 (released on 2003/01/01)
+<ul>
+<li>LDAP authentication module support
+<li>Add X-User-Real-E-Mail: header for real E-Mail address
+<li>Fix a typo error of default language file loading
+</ul>
+<p>
+PHP News Reader v1.8 (released on 2002/10/26)
+<ul>
+<li>Support encodings including Unicode/Chinese (UTF-8), Simplified Chinese (GB2312) and Traditional Chinese (BIG5).
+<li>Code conversion between any two of the above three common Chinese encodings
+<li>Interface re-formatting for various action
+<li>Fix a bug of forwarding article in english interface
+</ul>
+<p>
+PHP News Reader v1.7 (released on 2002/10/05)
+<ul>
+<li>Support Chinese bi-directional coding conversion of BIG5/GB2312
+<li>Support Chinese on-the-fly coding conversion while posting articles
+<li>Fix the multiple MIME encoding problem of the subject
+</ul>
+<p>
+PHP News Reader v1.6 (released on 2002/08/10)
+<ul>
+<li>Support square-bracketed catalog in newsgroups.lst
+<li>Couple of fixs for register_globals setting 'off'
+<li>HTML entities in Subject is now correctly escaped
+<li>Mail forwarding bug fixed
+<li>Check for valid E-Mail entered by user
+</ul>
+<p>
+PHP News Reader v1.5 (released on 2002/07/06)
+<ul>
+<li>Multiple news server
+<li>Run with PHP's register_globals setting turned off
+<li>Security enhancement for newsgroup verification
+<li>Switch interface language on the fly
+</ul>
+<p>
+PHP News Reader v1.4 (released on 2002/06/09)
+<ul>
+<li>Dynamic highlight of selected item 
+<li>Correct the problem about author information of XOVER, HEAD
+<li>Fix a bug of checking both "Not post to newsgroup" and "Reply to author"
+</ul>
+<hr />
+<table cellspacing=0 cellpadding=0 width=100%>
+<tr><td>
+<font size=2>$pnews_claim</font><br />
+<a href=http://sourceforge.net/projects/pnews/ target=_blank>http://sourceforge.net/projects/pnews/</a>
+</td><td align=right>
+<a href="http://sourceforge.net/" alt="http://sourceforge.net/" target=_blank>
+<img src="$sflogo" border="0" alt="SourceForge.net Logo">
+</a>
+</td></tr></table>
+</font>
+</body>
+</html>
+
+EOH;
+
+?>
---
+++ tags/PNEWS_2_4_0_RELEASE/pnews/examples/config.inc.php
@@ -0,0 +1,372 @@
+<?
+
+#
+# PHP News Reader Configuration File
+#
+# Copyright (C) 2001-2003 - All rights reserved
+# Shen Cheng-Da (cdsheen@users.sourceforge.net)
+
+# This is a sample configuration.
+# You should make a copy of this file at the directory of pnews.
+
+# After making a copy, you can begin editing this file,
+
+# In this configuration, you can use on as true, and off as false
+# Also note that no quote is needed, for example:  $foobar = on;
+
+define( 'on',  true,  true );
+define( 'off', false, true );
+
+# The configuration contains 3 sections:
+#
+#     Section 1 - Authentication
+#     Section 2 - Contents
+#     Section 3 - Languages
+
+# The setting with 'REQUIRED' mark and do not come with a default value
+# and should be explicitly configured
+
+############################################################################
+# Section 1 - Authentication - Configure how users login your system
+############################################################################
+
+
+# [auth_type] Specify the authentication type (REQUIRED)
+#  "required" - authentication is required to access the while system
+#  "optional" - authentication is only required for posting and forwarding
+#      "open" - authentication is not needed
+
+$CFG["auth_type"] = "optional";
+
+
+# [auth_prompt] Specify the style of login prompt (default: "form")
+#               (used when auth_type != 'open')
+#      "http" - authentication via HTTP login window    (default)
+#      "form" - authentication with login form
+
+$CFG["auth_prompt"] = "form";
+
+
+# [auth_http_realm] Specify the realm used in http authentication prompt
+#                   (REQUIRED if auth_prompt == 'http')
+
+$CFG["auth_http_realm"] = "XXX";
+
+
+# [auth_method] Specify the authentication method
+#               REQUIRED if auth_type != "open"
+#      "ldap" - authenticated via LDAP server,
+#      "pop3" - authenticated via POP3 server,
+#       "ftp" - authenticated via FTP server,
+#      "mail" - authenticated via multiple POP3 server,
+#     "mysql" - authenticated via MySQL database,
+#     "pgsql" - authenticated via PostgreSQL database,
+#      "nntp" - authenticated via NNTP server,
+#     "nntps" - authenticated via NNTPS server, (>=v2.4.0)
+#       "cas" - authenticated via CAS (>=v2.3.0)
+#      "user" - authenticated via your self-defined method,
+
+// $CFG["auth_method"] = "ftp";
+
+
+# [auth_organization] Specify the organization of authentication source
+#                     REQUIRED if auth_type != 'open'
+
+$CFG["auth_organization"] = "XXX Club";
+
+# [auth_registration_info] Tell users how to get an account (default: "")
+# This message will be displayed on login (FORM) or logout (HTTP) windows
+
+// $CFG["auth_registration_info"] = "You can register an account at <a hreg=http://foobar.com/>Nopy's Club</a>";
+
+
+# [auth_user_module] Specify user-defined authentication module location
+#                    REQUIRED if auth_method == 'user'
+
+// $CFG["auth_user_module"] = "auth/my_auth.inc.php";
+
+# You should implement a function with this prototype in your module:
+#
+#     function check_user_password( $username, $password )
+
+
+# LDAP authentication parameters (REQUIRED if auth_method == 'ldap')
+#    [ldap_server] LDAP server address ( address:port )
+#        [ldap_dn] LDAP distinguish name
+#  [ldap_bind_rdn] LDAP bind RDN, %u replaced by username (default: "%u")
+#  [ldap_bind_pwd] LDAP bind password, %p replaced by password (default: "%p")
+#    [ldap_filter] LDAP search filter (default: "(cn=%u)")
+#  [ldap_variable] The attributes extract from this LDAP search for later use
+#                  (default: null) ( %u can not be used here )
+
+// $CFG["ldap_server"]   = "ldap.domain.org";
+// $CFG["ldap_dn"]       = "ou=members, o=root";
+// $CFG["ldap_bind_rdn"] = "cn=%u,ou=members,o=root";
+// $CFG["ldap_bind_pwd"] = "%p";
+// $CFG["ldap_variable"] = array( "%e" => "Email", "%f" => "Fullname" );
+// $CFG["ldap_variable"] = array( "%e" => "Email" );
+// $CFG["ldap_filter"]   = "(&(cn=%u)(accountStatus=1)(MailStatus=1))";
+// $CFG["ldap_filter"]   = "(cn=%u)";
+
+
+# FTP authentication parameters (REQUIRED if auth_method == 'ftp')
+#      [$ftp_server] FTP server address ( address:port )
+#        [$ftp_deny] The user list which is denied for FTP authentication
+#                    (default: array( 'anonymous', 'guest', 'ftp' ) )
+
+// $CFG["ftp_server"]      = "ftp.domain.org";
+// $CFG["ftp_deny"]        = array( 'anonymous', 'guest', 'ftp', 'root' );
+
+
+# POP3 authentication parameters (REQUIRED if auth_method == 'pop3')
+#      [pop3_server] POP3 server address ( address:port )
+# [pop3_user_modify] fix the username used for POP3, %u replaced by username
+#                    (default: "%u")
+
+// $CFG["pop3_server"]      = "pop3.domain.org";
+// $CFG["pop3_user_modify"] = "%u";
+
+
+# Mail authentication parameters (REQUIRED if auth_method == 'mail')
+#     [pop3_mapping] the mapping from E-Mail to POP3 server address
+
+// $CFG["pop3_mapping"] = array( "@csie.nctu.edu.tw" => "pop3.csie.nctu.edu.tw",
+//				 "@mail.domain.org"  => "pop3.domain.org" );
+
+# MySQL/PostgreSQL Database authentication parameters
+# (REQUIRED if auth_method == 'mysql' || auth_method == 'pgsql' )
+
+#          [db_server] The database server address (address:port)
+#            [db_name] The database name
+#        [db_username] The username used to connect database
+#        [db_password] The password used to connect database
+#           [db_table] The table name for user information
+#  [db_field_username] The field name for username
+#  [db_field_password] The field name for password
+#  [db_password_crypt] The password encrypt method (default: "" - cleartext)
+#                      current supported are: "md5", "crypt"
+
+$CFG["db_server"]         = "database.domain.org";
+$CFG["db_name"]           = "phpbb";
+$CFG["db_username"]       = "db_user";
+$CFG["db_password"]       = "db_pass";
+$CFG["db_table"]          = "phpbb_users";
+$CFG["db_field_username"] = "username";
+$CFG["db_field_password"] = "user_password";
+$CFG["db_password_crypt"] = "md5";
+
+$CFG["db_variable"] = array( "%e" => "user_email" );
+
+# NNTP authentication parameters (REQUIRED if auth_method == 'nntp')
+#      [$auth_nntp_server] NNTP server address ( address:port )
+
+// $CFG["auth_nntp_server"]      = "news.domain.org";
+
+
+# NNTPS authentication parameters (REQUIRED if auth_method == 'nntps')
+#      [$auth_nntps_server] NNTPS server address ( address:port )
+
+// $CFG["auth_nntps_server"]      = "nntps.domain.org";
+
+
+# CAS authetication parameters (REQUIRED if auth_method == 'cas')
+#   [$auth_cas_server] CAS server FQDN and port ( address:port )
+#   [$auth_cas_base_uri] CAS server base URI
+#   [$auth_cas_debug] a file to debug phpCAS or FALSE to turn debugging off
+#                     (default: false)
+
+$CFG["auth_cas_server"] = 'cas.domain.name:8443';
+$CFG["auth_cas_base_uri"] = 'cas';
+
+// $CFG["auth_cas_debug"] = '/var/log/phpcas.log';
+
+# [auth_expire_time] After this time in seconds, authentication is expired
+#                    And login again is required. Zero means never expire
+#                    (default: 3600 seconds)
+
+$CFG["auth_expire_time"] = 1800;
+
+
+# [post_restriction] Disallow the posting and forwarding of articles
+#                    (default: off)
+
+// $CFG["post_restriction"] = on;
+
+
+# [auth_user_fullname]  The full name of authenticated user (default: "%u")
+
+// $CFG["auth_user_fullname"] = "%f";
+
+
+# [auth_user_email] The E-Mail of authenticated user (REQUIRED)
+#   The %u will be replaced by the username enter by authentication session
+#   Other variables are defined in the [db_variable] or [ldap_variable]
+#   (If you use 'mail' auth-method, %e will be the user's E-Mail,
+#    and %u will be the user name of the E-Mail - i.e: the string before '@')
+
+// $CFG["auth_user_email"] = "%u@mail.domain.com";
+// $CFG["auth_user_email"] = "%e";
+
+
+# [log] Enable access log (default: "" - no log)
+# You need to create this file w/ write permission to the user running httpd
+
+// $CFG["log"] = "/var/log/webnews.log";
+
+
+############################################################################
+# Section 2 - Contents - Configure the contents
+############################################################################
+
+# [url_base] The base url of your PHP News Reader installation (REQUIRED)
+
+# $CFG['url_base'] = 'http://webnews.foobar.com/news/';
+
+
+# [url_rewrite] Turn on URL rewrite (Need Apache's mod_rewrite support)
+#               (default: false)
+
+# $CFG['url_rewrite'] = true;
+
+
+# [https_login] Whether to use SSL(HTTPS) after authentication (default: off)
+
+# $CFG['https_login'] = true;
+
+
+# [style_sheet] Configure the style sheet (default: "standard.css")
+#               this style sheet must be reside in the css/ subdirectory.
+
+# $CFG['style_sheet'] = "fancy.css";
+
+
+# [title] The title for this webnews (default: "Webnews")
+
+$CFG["title"] = "XXX News Service";
+
+
+# [banner] The banner text or images, ex: "<img src=banner.jpg>" (default: "")
+
+// $CFG["banner"] = "<img src=banner.jpg>";
+
+
+# [group_list] The group definition that show on this Web News
+#              You should get a copy from examples/newsgroups.lst
+#              (default: "newsgroups.lst")
+
+// $CFG["group_list"] = "/somewhere/newsgroups.lst";
+
+
+# [magic_tag] Use magic tag to indicate the unread state of newsgroup
+#             (default: true)
+#
+
+// $CFG["magic_tag"] = false;
+
+
+# [cache_dir] Enable caching of indexing data   (default: false)
+#             To enable caching, set this to the directory of cache data.
+#             You should grant write permission on this directory to the
+#             user running PHP.
+
+// $CFG["cache_dir"] = "/tmp/pnews-cache";
+
+
+# [group_sorting] sort newsgroups in each catalog (default: off)
+
+// $CFG["group_sorting"] = on;
+
+
+# [articles_per_page] Number of articles shown in one single page (default: 20)
+
+// $CFG['articles_per_page'] = 30;
+
+
+# [show_newest_top] Whether to show the newest article as the top item (default: on)
+
+// $CFG["show_newest_top"] = off;
+
+
+# [show_article_popup] Show article in pupup window (default: off)
+
+// $CFG["show_article_popup"] = on;
+
+
+# [email_editing] Allow E-Mail editing when posting article (default: on)
+
+// $CFG["email_editing"] = off;
+
+
+# [filter_ansi_color] Show article in pupup window (default: on)
+
+// $CFG["filter_ansi_color"] = off;
+
+
+# [time_format] The format used to displaying time (default: "%Y/%m/%d %H:%M:%S")
+
+// $CFG["time_format"] = "%Y/%m/%d %H:%M:%S";
+
+
+# [style_sheet] Alternative CSS (default: "style.css")
+
+// $CFG["style_sheet"] = 'my_style.css';
+
+
+# [language_switch] Show language switch or not (default: on)
+
+// $CFG["language_switch"] = off;
+
+
+# [organization] The organization name of this site (default: "News Server")
+
+$CFG["organization"] = "XXX News Server";
+
+
+# [post_signature] The signature to appended at each posted article
+#                  (default: "")
+
+// $CFG["post_signature"] = "\n-- \nPOST BY: PHP News Reader\n";
+
+
+# [show_sourceforge_logo] Show SourceForge logo (default: true)
+
+// $CFG["show_sourceforge_logo"] = false;
+
+# [links] The links referring to other pages, (default: null)
+
+// $CFG["links"] = array( "Back Home" => "../index.php",
+//			  "Tech News" => "http://foo.bar/technews/" );
+
+
+############################################################################
+# Section 3 - Languages - Configure the setting about languages
+############################################################################
+#
+#    Natively supported languages and the codings are:
+#        "en" - Englush                    US-ASCII
+#     "zh-tw" - Traditional Chinese        BIG5
+#     "zh-cn" - Simplified Chinese         GB2312
+#   "unicode" - Unicode                    UTF-8       (default)
+#        "fr" - French                     ISO-8859-1
+
+# [interface_language] The language setting of interface (default: "en")
+
+$CFG["interface_language"] = "en";
+
+# [charset] The charset setting for various resource (default: "utf-8")
+#             [config] The language setting used in this config.inc.php
+#           [grouplst] The language setting used in newsgroups.lst
+#           [database] The language setting used in database or LDAP
+
+$CFG["charset"]["config"]    = "big5";
+$CFG["charset"]["grouplst"]  = "big5";
+$CFG["charset"]["database"]  = "big5";
+
+
+############################################################################
+# PHP News Reader
+# Copyright (C) 2001-2003 - All rights reserved
+# Shen Cheng-Da (cdsheen@csie.nctu.edu.tw)
+############################################################################
+
+?>
