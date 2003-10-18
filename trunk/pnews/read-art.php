cdf6514507f00832b9a6f440e88dc20c1918e69b
cdsheen <cdsheen@fe2ed6f1-d525-0410-ac78-8749a169f707>
2003-10-18 11:52:52 +0000

    no border, fix for fancy css
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@287 fe2ed6f1-d525-0410-ac78-8749a169f707

--- trunk/pnews/read-art.php
+++ trunk/pnews/read-art.php
@@ -112,8 +112,10 @@ if( strlen( $org ) > $org_limit )
 
 echo "<center>\n";
 
-#if( !$CFG['show_article_popup'] ) {
-	echo "<table width=100% border=1 cellpadding=2 cellspacing=0>";
+
+	echo "<table class=shadow border=0 width=100% cellpadding=0 cellspacing=0>\n";
+	echo "<tr><td class=bg>\n";
+	echo "<table width=100% border=0 cellpadding=2 cellspacing=2>";
 
 	if( $CFG['show_article_popup'] ) {
 		echo "<tr><td class=group>\n";
@@ -169,6 +171,8 @@ echo "</table>";
 
 toolbar( $server, $group, $artnum, $subject );
 
+echo "</td></tr></table>\n";
+
 echo "</center>";
 html_foot(false);
 html_tail();
@@ -179,7 +183,7 @@ function toolbar( $server, $group, $artnum, $title ) {
 	global $strCrossPost, $strForward, $strDelete;
 	global $strMyFavor, $strReturnToIndexing, $strNextArticle, $strLastArticle;
 	global $CFG, $nexturl, $lasturl, $idxurl;
-	echo "<table width=100% border=1 cellspacing=0 cellpadding=2>\n";
+	echo "<table width=100% border=0 cellspacing=2 cellpadding=2>\n";
 	if( !$post_restriction ) {
 		echo "<tr>";
 		if( ! $CFG['show_article_popup'] ) {
