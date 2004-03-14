ebbb4d841cf19b44c6fc8126362a770dc5a8ddcb
(no author) <(no author)@fe2ed6f1-d525-0410-ac78-8749a169f707>
2004-03-14 16:16:35 +0000

    This commit was manufactured by cvs2svn to create tag 'PNEWS_2_5_5_RELEASE'.
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@451 fe2ed6f1-d525-0410-ac78-8749a169f707

---
+++ tags/PNEWS_2_5_5_RELEASE/pnews/language/german.inc.php
@@ -0,0 +1,122 @@
+<?
+
+// PHP News Reader - Language definition
+
+// Language: German (Germany)
+// Coding:   ISO-8859-1
+
+// Copyright (C) 2001-2004 - All rights reserved
+// Jochen Staerk (jstaerk at usegroup.de)
+
+$strAuthentication     = 'Login';
+$strLoginName          = 'Login-Name';
+$strPassWord           = 'Passwort';
+$strNeedLogin          = 'Die angefragte Seite erfordert eine Authentifizierung';
+$strUseYourAccountAt   = 'Bitte nutzen Sie Ihren Account bei <b>%s</b> um einzuloggen:';
+$strLogin              = 'Login';
+$strLogout             = 'Logout';
+
+$strHTTPlogoutInfo     = 'Bitte schlie&szlig;en Sie alle Browserfenster um unauthorisierte Zugriff zu vermeiden.';
+
+$strAuthExpired        = 'Zeit abgelaufen! Sie sollten sich erneut einloggen!';
+
+$strAuthFail           = 'Authentifizierung fehlgeschlagen';
+$strReadyReturn        = 'Bereit um auf die urspr&uuml;ngliche Seite zur&uuml;ckzukehren, oder klicken Sie <a href="%s">hier</a> um jetzt zur&uml;ckzukehren';
+$strPasswordRetryFail  = 'Angegebener Nutzername und Passwort sind nicht korrekt';
+$strReLogin            = 'Wieder einloggen';
+
+$strGoBack             = 'Zur&uuml;ck';
+
+$strNewsServer         = 'News-Server';
+$strConnectServerError = 'Fehler bei der Verbindung zum News-Server.';
+$strReadonlyNotify     = 'Nur-Lesen-Modus';
+
+$strGroupList          = 'Gruppenliste';
+
+$strSessionError       = 'Zugriffsfehler';
+$strSessionErrorReason = 'Zugriff %s fehler - nur-lesen Zugriff oder Problem mit dem Timeout';
+$strSessionErrorReturn = 'Bitte gehen Sie zu %s zur&uuml;ck und versuchen Sie es noch einmal';
+
+$strNumber             = 'Nr';
+$strPostNumber         = 'Arikel #';
+$strGroup              = 'Newsgroup';
+$strGroupDescription   = 'Beschreibung';
+$strSubject            = 'Betreff';
+$strAuthor             = 'Autor';
+$strTime               = 'Datum';
+$strAction             = 'Aktion';
+$strReceiver           = 'Empf&auml;nger';
+$strName               = 'Name';
+$strEmail              = 'E-Mail';
+$strContent            = 'Inhalt';
+$strAttachment         = 'Anhang';
+
+$strNoSubject          = '<kein Betreff>';
+
+$strRefresh            = 'Neu laden';
+
+$strReply              = 'Antworten';
+$strDelete             = 'L&ouml;schen';
+$strCrossPost          = 'Crosspost';
+$strForward            = 'Weiterleiten';
+
+$strPost               = 'Neuer Artikel';
+
+$strCloseWindow        = 'Schlie&szlig;en';
+
+$strShowHeader         = 'Nachrichtenkopf anzeigen';
+
+$strFirstPage          = 'Erste';
+$strLastPage           = 'Letzte';
+$strNextPage           = 'N&auml;chste';
+$strPreviousPage       = 'Vorige';
+
+$strNextArticle        = 'N&auml;chster';
+$strLastArticle        = 'Voriger'; 
+
+$strPageNumber         = 'Seite %s von %s Seiten';
+
+$strReturnToGroupList  = 'Gruppenliste';
+$strReturnToIndexing   = 'Artikelliste';
+$strNoPostPermission   = 'Sie haben keine Berechtigung, in dieser Gruppe zu posten';
+$strNoArticle          = 'Keine Artikel in dieser Newsgroup';
+
+$strArticlePosted      = 'Der folgende Artikel wurde versandt';
+$strArticleIsDeleted   = 'Der folgende Artikel wurde gel&ouml;scht';
+$strArticleIsForwarded = 'Der folgende Artikel wurde weiter geleitet';
+
+$strRealyQuit          = 'Bearbeiten wirklich beenden ?';
+$strRealyDelete        = 'Diesen Artikel wirklich l&ouml;schen ?';
+
+$strFormConfirmPost    = 'Best&auml;tigen';
+$strFormCancelPost     = 'Senden abbrechen';
+
+$strFormConfirmDelete  = 'L&ouml;schen';
+$strFormCancelDelete   = 'L&ouml;schen abbrechen';
+
+$strFormConfirmForward = 'Weiterleiten';
+$strFormCancelForward  = 'Weiterleiten abbrechen';
+
+$strFormInsertQuote    = 'Text kopieren';
+
+$strPleaseEnterName    = 'Bitte geben Sie Ihren Namen an!';
+$strPleaseEnterEmail   = 'Bitte geben Sie Ihre E-Mail-Adresse an!';
+$strPleaseEnterGroup   = 'Bitte geben Sie die Newsgroup an!';
+$strPleaseEnterSubject = 'Bitte geben Sie den Betreff an!';
+$strPleaseEnterContent = 'Bitte geben Sie Inhalt ein!';
+$strPleaseEnterReceiver= 'Bitte geben Sie die E-Mail-Adresse des Empf&auml;ngers an!';
+
+$strNoPostJustMail     = 'M&ouml;chten Sie nur mit Mail ohne zu Posten antworten ?';
+
+$strReplyToAuthor      = 'Autor antworten';
+$strNotPostToGroup     = 'Nicht posten';
+
+$strQuoteFrom          = '== Auszug aus %s\'s Artikel';
+$strForwardFrom        = '== Weitergeleitet durch %s';
+$strCrossPostAuthor    = '== Neu gepostet durch %s';
+
+$strPostStatus         = '== Postet an %s durch %s';
+
+$strMyFavor            = 'Lesezeichen';
+
+?>
