9b35beedbb8f1fe47dd39b0c8ab018277cddd18c
(no author) <(no author)@fe2ed6f1-d525-0410-ac78-8749a169f707>
2004-08-04 15:14:14 +0000

    This commit was manufactured by cvs2svn to create tag 'PNEWS_2_5_8_RELEASE'.
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@490 fe2ed6f1-d525-0410-ac78-8749a169f707

---
+++ tags/PNEWS_2_5_8_RELEASE/pnews/language/finnish.inc.php
@@ -0,0 +1,122 @@
+<?
+
+// PHP News Reader - Language definition
+
+// Language: Finnish
+// Coding:   ISO-8859-1
+
+// Copyright (C) 2001-2004 - All rights reserved
+// Markus Översti (markus.oversti at edu.haapavesi.fi)
+
+$strAuthentication     = 'Kirjaudu';
+$strLoginName          = 'Käyttäjänimi';
+$strPassWord           = 'Salasana';
+$strNeedLogin          = 'Haluamasi sivu vaatii sisäänkirjautumista';
+$strUseYourAccountAt   = 'Käytä palvelun <b>%s</b> tunnusta!';
+$strLogin              = 'Kirjaudu';
+$strLogout             = 'Kirjaudu ulos';
+
+$strHTTPlogoutInfo     = 'Sulje kaikki selainikkunat estääksesi luvattoman käytön.';
+
+$strAuthExpired        = 'Sisäänkirjautuminen on vanhentunut! Kirjaudu sisään uudelleen!';
+
+$strAuthFail           = 'Väärä salasana!';
+$strReadyReturn        = 'Valmis palaamaan edelliselle sivulle, tai paina <a href="%s">tästä</a> palataksesi heti.';
+$strPasswordRetryFail  = 'Käyttäjänimi ja/tai salasana ovat vääriä!';
+$strReLogin            = 'Kirjaudu uudestaan';
+
+$strGoBack             = 'Palaa takaisin';
+
+$strNewsServer         = 'Uutispalvelin';
+$strConnectServerError = 'Uutispalvelimelle kirjautuminen epäonnistui.';
+$strReadonlyNotify     = 'Vain luku';
+
+$strGroupList          = 'Ryhmälista';
+
+$strSessionError       = 'Lukuvirhe';
+$strSessionErrorReason = 'Tapahtui %s virhe - vain luku tai aikarajoitus';
+$strSessionErrorReturn = 'Palaa sivulle %s ja yritä uudelleen';
+
+$strNumber             = 'Numero';
+$strPostNumber         = 'Lkm #';
+$strGroup              = 'Uutisryhmä';
+$strGroupDescription   = 'Kuvaus';
+$strSubject            = 'Aihe';
+$strAuthor             = 'Kirjoittaja';
+$strTime               = 'Aika';
+$strAction             = 'Toiminto';
+$strReceiver           = 'Vastaanottaja';
+$strName               = 'Nimi';
+$strEmail              = 'Sähköposti';
+$strContent            = 'Sisältö';
+$strAttachment         = 'Liite';
+
+$strNoSubject          = '<ei otsikkoa>';
+
+$strRefresh            = 'Päivitä';
+
+$strReply              = 'Vastaa';
+$strDelete             = 'Poista';
+$strCrossPost          = 'Lähetä useaan ryhmään';
+$strForward            = 'Lähetä edelleen';
+
+$strPost               = 'Lähetä artikkeli';
+
+$strCloseWindow        = 'Sulje';
+
+$strShowHeader         = 'Näytä otsakkeet';
+
+$strFirstPage          = 'Ensimmäinen';
+$strLastPage           = 'Viimeinen';
+$strNextPage           = 'Seuraava';
+$strPreviousPage       = 'Edellinen';
+
+$strNextArticle        = 'Seuraava';
+$strLastArticle        = 'Edellinen'; 
+
+$strPageNumber         = 'Sivu %s kaikkiaan %s sivusta';
+
+$strReturnToGroupList  = 'Ryhmien luettelo';
+$strReturnToIndexing   = 'Artikkelilista';
+$strNoPostPermission   = 'Sinulla ei ole oikeuksia lähettää artikkelia tähän ryhmään!';
+$strNoArticle          = 'Uutisryhmässä ei ole artikkeleita';
+
+$strArticlePosted      = 'Artikkeli on lähetetty uutisryhmiin';
+$strArticleIsDeleted   = 'Artikkeli on poistettu';
+$strArticleIsForwarded = 'Artikkeli on lähetetty edelleen';
+
+$strRealyQuit          = 'Tahdotko lopettaa muokkaamisen?';
+$strRealyDelete        = 'Tahdotko todella tuhota artikkelin?';
+
+$strFormConfirmPost    = 'Lähetä uutisryhmiin';
+$strFormCancelPost     = 'Peru uutisryhmiin lähetys';
+
+$strFormConfirmDelete  = 'Poista';
+$strFormCancelDelete   = 'Peru';
+
+$strFormConfirmForward = 'Jatka';
+$strFormCancelForward  = 'Peru';
+
+$strFormInsertQuote    = 'Kopioi teksti';
+
+$strPleaseEnterName    = 'Kirjoita nimesi!';
+$strPleaseEnterEmail   = 'Kirjoita sähköpostiosoitteesi!';
+$strPleaseEnterGroup   = 'Anna uutisryhmä!';
+$strPleaseEnterSubject = 'Kirjoita otsikko!';
+$strPleaseEnterContent = 'Kirjoita viestiin sisältöä!';
+$strPleaseEnterReceiver= 'Anna vastaanottajan sähköpostiosoite!';
+
+$strNoPostJustMail     = 'Tahdotko lähettää vastausviestin ilman viestin lähetystä uutisryhmiin?';
+
+$strReplyToAuthor      = 'Vastaa kirjoittajalle sähköpostitse';
+$strNotPostToGroup     = 'Älä lähetä uutisryhmiin';
+
+$strQuoteFrom          = '== Lainaus %s\:lta';
+$strForwardFrom        = '== Lähetetty edelleen %s toimesta';
+$strCrossPostAuthor    = '== Vastausviesti %s:lta';
+
+$strPostStatus         = '== Lähetetty %s vastaanottajalle %s';
+
+$strMyFavor            = 'Lisää suosikkeihin';
+
+?>
---
+++ tags/PNEWS_2_5_8_RELEASE/pnews/language/slovak.inc.php
@@ -0,0 +1,122 @@
+<?
+
+// PHP News Reader - Language definition
+
+// Language: Slovak
+// Coding:   ISO-8859-2
+
+// Copyright (C) 2001-2004 - All rights reserved
+// tichu (jahu at gentoo.sk)
+
+$strAuthentication     = 'Prihlásenie';
+$strLoginName          = 'Prihlasovacie meno';
+$strPassWord           = 'Heslo';
+$strNeedLogin          = 'Po¾adovaná stránka vy¾aduje autentizáciu';
+$strUseYourAccountAt   = 'Pre prihlásenie prosím pou¾ite va¹e prihlasovacie údaje na poèítaèi <b>%s</b>:';
+$strLogin              = 'Prihlási»';
+$strLogout             = 'Odhlási»';
+
+$strHTTPlogoutInfo     = 'Prosím zatvorte v¹etky okná internetového prehliadaèa, aby sa zamedzilo nepovolanému prístupu.';
+
+$strAuthExpired        = 'Èas vypr¹al! Budete sa musie» prihlási» znova!';
+
+$strAuthFail           = 'Autentizácia bola neúspe¹ná';
+$strReadyReturn        = 'Poèkajte na návrat na pôvodnú stránku, alebo kliknite <a href="%s">sem</a> pre okam¾itý návrat';
+$strPasswordRetryFail  = 'Zadané prihlasovacie meno a heslo sú nesprávne';
+$strReLogin            = 'Prihlási» znovu';
+
+$strGoBack             = 'Ís» naspä»';
+
+$strNewsServer         = 'News Server';
+$strConnectServerError = 'Pripojenie k news serveru sa nepodarilo';
+$strReadonlyNotify     = 'Prístup len na èítanie';
+
+$strGroupList          = 'Zoznam skupín';
+
+$strSessionError       = 'Chyba prístupu';
+$strSessionErrorReason = 'Chyba prístupu %s - prístup len na èítanie, alebo vypr¹aný èas';
+$strSessionErrorReturn = 'Vrá»te sa na %s a skúste znovu';
+
+$strNumber             = 'Èíslo';
+$strPostNumber         = 'Poèet prísp.';
+$strGroup              = 'Skupina';
+$strGroupDescription   = 'Popis';
+$strSubject            = 'Téma';
+$strAuthor             = 'Autor';
+$strTime               = 'Èas';
+$strAction             = 'Akcia';
+$strReceiver           = 'Príjemca';
+$strName               = 'Meno';
+$strEmail              = 'E-Mail';
+$strContent            = 'Content';
+$strAttachment         = 'Attachment';
+
+$strNoSubject          = '<bez témy>';
+
+$strRefresh            = 'Obnovi»';
+
+$strReply              = 'Odpoveda»';
+$strDelete             = 'Zmaza»';
+$strCrossPost          = 'Crosspost';
+$strForward            = 'Preposla»';
+
+$strPost               = 'Odosla»';
+
+$strCloseWindow        = 'Zavrie»';
+
+$strShowHeader         = 'Zobrazi» hlavièky';
+
+$strFirstPage          = 'Prvá';
+$strLastPage           = 'Posledná';
+$strNextPage           = 'Ïal¹ia';
+$strPreviousPage       = 'Predch.';
+
+$strNextArticle        = 'Ïal¹í';
+$strLastArticle        = 'Predch.'; 
+
+$strPageNumber         = 'Stránka %s z %s';
+
+$strReturnToGroupList  = 'Zoznam skupín';
+$strReturnToIndexing   = 'Zoznam príspevkov';
+$strNoPostPermission   = 'Do tejto skupiny nemáte právo posla» príspevok';
+$strNoArticle          = 'V tejto skupine nie sú ¾iadne príspevky';
+
+$strArticlePosted      = 'Bol odoslaný nasledujúci príspevok';
+$strArticleIsDeleted   = 'Bol zmazaný nasledujúci príspevok';
+$strArticleIsForwarded = 'Bol preposlaný nasledujúci príspevok';
+
+$strRealyQuit          = 'Naozaj chcete ukonèi» úpravu ?';
+$strRealyDelete        = 'Naozaj chcete zmaza» tento príspevok ?';
+
+$strFormConfirmPost    = 'Odosla»';
+$strFormCancelPost     = 'Zru¹i»';
+
+$strFormConfirmDelete  = 'Zmaza»';
+$strFormCancelDelete   = 'Zru¹i»';
+
+$strFormConfirmForward = 'Preposla»';
+$strFormCancelForward  = 'Zru¹i»';
+
+$strFormInsertQuote    = 'Citova»';
+
+$strPleaseEnterName    = 'Nezadali ste meno!';
+$strPleaseEnterEmail   = 'Nezadali ste svoj e-mailovú adresu!';
+$strPleaseEnterGroup   = 'Neurèili ste skupinu!';
+$strPleaseEnterSubject = 'Nezadali ste tému!';
+$strPleaseEnterContent = 'Príspevok je prázdny!';
+$strPleaseEnterReceiver= 'Nezadali ste e-mailovú adresu príjemcu!';
+
+$strNoPostJustMail     = 'Naozaj chcete posla» len email autorovi ?';
+
+$strReplyToAuthor      = 'Odpoveda» autorovi';
+$strNotPostToGroup     = 'Neposiela» príspevok do skupiny';
+
+$strQuoteFrom          = '== %s napísal';
+$strForwardFrom        = '== %s preposlal';
+$strCrossPostAuthor    = '== %s napísal inde';
+
+$strPostStatus         = '== Napísané %s do %s';
+
+$strMyFavor            = 'Obµúbené';
+
+?>
