5a94fbedd75f9c596a269e42d487621daa9999b4
cdsheen <cdsheen@fe2ed6f1-d525-0410-ac78-8749a169f707>
2004-08-11 03:31:54 +0000

    v2.5.9 bump
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@495 fe2ed6f1-d525-0410-ac78-8749a169f707

--- trunk/pnews/version.inc.php
+++ trunk/pnews/version.inc.php
@@ -20,8 +20,8 @@
 # PHP News Reader - Version Information
 
 $pnews_name      = 'PHP News Reader';
-$pnews_version   = 'v2.5.8';
-$pnews_release   = '2004/08/05';
+$pnews_version   = 'v2.5.9';
+$pnews_release   = '2004/08/11';
 $pnews_copyright = '(C) 2001-2004 - Shen Cheng-Da';
 $pnews_claim     = "$pnews_name $pnews_version $pnews_copyright";
 
