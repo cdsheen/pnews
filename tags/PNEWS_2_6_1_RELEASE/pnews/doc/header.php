83d26c11d1e66976decde4dd33039f39af9892e8
(no author) <(no author)@fe2ed6f1-d525-0410-ac78-8749a169f707>
2005-01-01 08:34:03 +0000

    This commit was manufactured by cvs2svn to create tag 'PNEWS_2_6_1_RELEASE'.
    git-svn-id: http://pnews.svn.sourceforge.net/svnroot/pnews@544 fe2ed6f1-d525-0410-ac78-8749a169f707

---
+++ tags/PNEWS_2_6_1_RELEASE/pnews/doc/header.php
@@ -0,0 +1,49 @@
+<?
+
+include_once('../version.inc.php');
+
+$menus = array( 'index.php' => 'Introduction',
+		'guide.php' => 'Documentation',
+		'history.php' => 'Changes and History',
+		'download.php' => 'Download' );
+
+$titles = array( 'index.php' => 'PHP News Reader - Introduction',
+		'guide.php' => 'PHP News Reader - Installation and Configuration Documentation',
+		'history.php' => 'PHP News Reader - Release notes and Histroy',
+		'download.php' => 'PHP News Reader - Download',
+		'requirement.php' => 'PHP News Reader - Requirement',
+		'acknowlege.php' => 'PHP News Reader - Acknowlegement',
+		'url_rewrite.php' => 'PHP News Reader - URL Rewriting with Apache/mod_rewrite' );
+
+echo <<<REL
+<table width=100% cellpadding=0 cellspacing=0>
+<tr>
+ <td>
+ <font face="Georgia"><h3>$pnews_name $pnews_version</h3></font>
+ </td>
+ <td align=right valign=bottum>
+ <font face="Georgia" size=1>Release Date: $pnews_release</font>
+ </td></tr>
+</table>
+REL;
+	$curr_scr = basename($_SERVER['PHP_SELF']);
+//	echo $curr_scr;
+	$title = $titles[$curr_scr];
+
+	echo "<table width=100%>\n<tr><td align=left><font size=3 face=Georgia>$title</font>";
+	echo "</td><td align=right>\n";
+	echo "<table cellpadding=0 cellspacing=1>\n<tr><td bgcolor=black>";
+	echo "<table cellpadding=3 cellspacing=2 style='font-family: Georgia'>\n";
+	echo "<tr bgcolor=white>\n";
+#	echo "<td><font size=3 color=black>PHP News Reader</font></td>\n";
+	foreach( $menus as $scr => $menu ) {
+		if( $curr_scr == $scr )
+			echo "<td align=25% bgcolor=black><font size=3 color=white>$menu</font></td>\n";
+		else
+			echo "<td align=25% bgcolor=#FFFFA0 onMouseover='this.bgColor=\"#CCCCFF\"' onMouseout='this.bgColor=\"#FFFFA0\"'><a href=$scr><font size=3>$menu</font></a></td>\n";
+	}
+	echo "</tr>\n";
+	echo "</table>\n";
+	echo "</td></tr></table>\n";
+	echo "</td></tr></table>\n<hr />\n";
+?>
